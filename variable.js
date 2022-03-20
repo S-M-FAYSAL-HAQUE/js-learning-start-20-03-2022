376e1dd626070dd7be67f102c390dd8e2be5a1a6
S-M-FAYSAL-HAQUE <s.m.fh007@gmail.com>
2022-03-20 12:16:45 +0600

    update variable type and their writting process

--- variable.js
+++ variable.js
@@ -1,14 +1,46 @@
+// numeric variable
 var price = 50;
 var age = 28;
 var temp =39;
 var weight = 126;
 console.log(price, age, temp, weight);
 
-// ***--- how to write a variable ---***
+// string variable
+var name = "fazlul haque";
+var address = 'West Manikdi';
+var gf = "nahid pervin";
 
-// variable likhte 5 ta jinish lage:
+// boolean variable
+var pass = true;
+var pass = false;
+
+var subscribed = true;
+var subscribed = false;
+
+// ***--- how to write a numeric variable ---***
+
+// numeric variable likhte 5 ta jinish lage:
 // 1. variable ar short form "var" likhte hoy.
-// 2. variable ar ekta nam dite hoy. like, "price/age/name/weight/a/b/x/y" etc.
+// 2. variable ar ekta nam dite hoy. like, "price/age/weight/a/b/x/y" etc.
 // 3. erpor equal "=" operator dite hoy.
 // 4. operator ar pore kono variable ar "value" dite hoy. like, "var height = 10".
 // 5. shob seshe ";" semicolon dite hoy. 
+
+// ***--- how to write a string variable ---***
+
+// boolean variable likhte 5 ta jinish lage:
+// 1. variable ar short form "var" likhte hoy.
+// 2. variable ar ekta nam dite hoy. like, "price/age/name/address/(any kind of name)/weight/a/b/x/y" etc.
+// 3. erpor equal "=" operator dite hoy.
+// 4. operator ar pore kono variable ar value/kono nam/character dite hoy. like, "var height = 10/fahad/india/@!lima" etc.
+// 5. single/double quotation ("")/('') ar moddhe value/kono nam/character egula likhte hobe.
+// 6. shob seshe ";" semicolon dite hoy.
+
+// ***--- how to write a boolean variable ---***
+
+// boolean variable likhte 5 ta jinish lage:
+// 1. variable ar short form "var" likhte hoy.
+// 2. variable ar ekta nam dite hoy. like, "price/age/weight/a/b/x/y/pass/subscribe" etc.
+// 3. erpor equal "=" operator dite hoy.
+// 4. operator ar pore kono variable ar "value" dite hoy. like, var height = true/false. true/false k kono quotation ar moddhe rakha jabe na.
+// 5. shob seshe ";" semicolon dite hoy.
