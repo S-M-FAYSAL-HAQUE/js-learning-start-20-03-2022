3c395b3dd7b7e55f9d99430981ec4c50e499ff20
S-M-FAYSAL-HAQUE <s.m.fh007@gmail.com>
2022-03-20 12:57:42 +0600

    how to write variable name and what is the good practice to write

---
+++ variableName.js
@@ -0,0 +1,12 @@
+// ***--- how to write variable name ---***
+
+// JavaScript case sensitive.
+
+// Good practice for writing variable name:
+var name = "faysal";
+var userName = "Fahad"; [camel_case]
+var PIE = 3.1416 (universal_truth_ar_jonno_capital_letter_use_kora_jay)
+
+// another way to write variable name but this is not good practice:
+var user_address = "239/1, West Manikdi";
+var food99 = "apple";
