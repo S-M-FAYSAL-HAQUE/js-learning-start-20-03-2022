2e8eeb368a2587ff26b926dcd0d2fd86675f4a6b
S-M-FAYSAL-HAQUE <s.m.fh007@gmail.com>
2022-03-21 02:57:55 +0600

    variable types

---
+++ variableTypes.js
@@ -0,0 +1,15 @@
+// string type
+var name = "faysal";
+console.log(typeof name);
+
+// number type
+var price = 50;
+console.log(typeof price);
+
+// boolean type
+var pass = true;
+console.log(typeof pass);
+
+// object type
+var date = new Date();
+console.log(typeof date);
