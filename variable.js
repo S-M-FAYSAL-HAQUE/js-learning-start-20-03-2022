ac5e3559aeea573f50ea5026df3eb65c4f72392f
S-M-FAYSAL-HAQUE <s.m.fh007@gmail.com>
2022-03-20 11:22:54 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+var price = 50;
+var age = 28;
+var temp =39;
+var weight = 126;
+console.log(price, age, temp, weight);
+
+// ***--- how to write a variable ---***
+
+// variable likhte 5 ta jinish lage:
+// 1. variable ar short form "var" likhte hoy.
+// 2. variable ar ekta nam dite hoy. like, "price/age/name/weight/a/b/x/y" etc.
+// 3. erpor equal "=" operator dite hoy.
+// 4. operator ar pore kono variable ar "value" dite hoy. like, "var height = 10".
+// 5. shob seshe ";" semicolon dite hoy. 
