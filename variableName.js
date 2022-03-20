f9a0ddc2b6ce83e5c3617e2aba0d555872baeb5b
S-M-FAYSAL-HAQUE <s.m.fh007@gmail.com>
2022-03-21 03:59:50 +0600

    update good practice

--- variableName.js
+++ variableName.js
@@ -4,11 +4,12 @@
 
 // Good practice for writing variable name:
 var name = "faysal";
-var userName = "Fahad"; [camel_case]
-var PIE = 3.1416 (universal_truth_ar_jonno_capital_letter_use_kora_jay)
+var userName = "Fahad"; //camel_case
+var food99 = "apple";
+var PIE = 3.1416 //universal_truth_ar_jonno_capital_letter_use_kora_jay
 
 // another way to write variable name but this is not good practice:
 var user_address = "239/1, West Manikdi";
-var food99 = "apple";
+
 
 // js ar "keyword" gulo kono din variable name hishebe neya jabe na.
