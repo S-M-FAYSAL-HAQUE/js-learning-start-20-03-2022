57e05c898ae3a6f9f5d5f0e7437cf16a6e28efd9
S-M-FAYSAL-HAQUE <s.m.fh007@gmail.com>
2022-03-21 20:48:01 +0600

    how to write array and find array length.

---
+++ array.js
@@ -0,0 +1,15 @@
+// ***--- how to write array ---***
+
+// integer array
+
+// variable declare kore "[]" er majhe integer/float number or string likhte hoy.
+
+var friendsAges = [11, 15, 17, 10];
+var ages = friendsAges;
+var picnicFee = [200, 150, 500];
+console.log(friendsAges);
+console.log(friendsAges.length); // array ar moddhe koyta upadan ache seta dekhay ".length" diye.
+
+// string array
+
+var friendsName = ['faysal', 'fahad', 'nayeem', 'noyon', 'fahim'];
